56732c09fd4617d39feae8e1b401f42156162772
aruneJacademy <157484172+aruneJacademy@users.noreply.github.com>
2025-01-07 22:59:41 +0000

    evil mushroom ai not working yet

--- Source/ShroomCafe/WFCManager.cpp
+++ Source/ShroomCafe/WFCManager.cpp
@@ -170,10 +170,10 @@ void AWFCManager::OnProceduralGeneration()
 	InitializeGrid();
 
 	// WFC Algorithm
-	GenerateGrid();
+	//GenerateGrid();
 
 	// Spawn collapsed tiles in the world
-	SpawnGrid();
+	//SpawnGrid();
 }
 
 
